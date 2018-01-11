542824265a1329d939ca1165178d81acfe37a659
brucewu-fly <brucewu.fly.cn@gmail.com>
2018-01-11 14:33:35 +0800

    Add travis

--- src/test/java/com/aliyun/openservices/log/log4j2/LoghubAppenderTest.java
+++ src/test/java/com/aliyun/openservices/log/log4j2/LoghubAppenderTest.java
@@ -1,26 +1,57 @@
 package com.aliyun.openservices.log.log4j2;
 
+import com.aliyun.openservices.log.producer.ProducerConfig;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.status.StatusData;
+import org.apache.logging.log4j.status.StatusLogger;
+import org.junit.AfterClass;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotEquals;
 
 public class LoghubAppenderTest {
 
-    private static Logger logger = LogManager.getLogger("slsLogger");
+    private static final Logger LOGGER = LogManager.getLogger(LoghubAppenderTest.class);
+
+    private static void sleep() {
+        ProducerConfig producerConfig = new ProducerConfig();
+        try {
+            Thread.sleep(2 * producerConfig.packageTimeoutInMS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void checkStatusDataList() {
+        sleep();
+        List<StatusData> statusDataList = StatusLogger.getLogger().getStatusData();
+        for (StatusData statusData : statusDataList) {
+            statusData.getLevel();
+        }
+        for (StatusData statusData : statusDataList) {
+            Level level = statusData.getLevel();
+            assertNotEquals(statusData.getMessage().toString(), Level.ERROR, level);
+        }
+    }
 
     @Test
     public void testLogCommonMessage() {
-        logger.warn("This is a test common message logged by log4j2.");
+        LOGGER.warn("This is a test common message logged by log4j2.");
     }
 
     @Test
     public void testLogThrowable() {
-        logger.error("This is a test error message logged by log4j2.",
-                new UnsupportedOperationException("log4j2 appender test."));
+        LOGGER.error("This is a test error message logged by log4j2.",
+                new UnsupportedOperationException("Log4j2 UnsupportedOperationException"));
     }
 
     @Test
     public void testLogLevelInfo() {
-        logger.info("This is a test error message logged by log4j2, level is info,should not be logged.");
+        LOGGER.info("This is a test error message logged by log4j2, level is info,should not be logged.");
     }
 }
